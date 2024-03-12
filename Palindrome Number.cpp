549d1f6082bf266fb57ec403478b7a5496152631
Haroon Raza <146657522+voltvirtuoso@users.noreply.github.com>
2024-03-12 19:56:31 +0500

    Create Palindrome Number.cpp

---
+++ Palindrome Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+        long long reversed = 0, original = x;
+        while (x > 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+};
